2d2e43669501fe2ad70068aacc4f586cab1ca074
YTRodi <yagorodi@gmail.com>
2019-12-10 11:39:43 -0300

    lele

---
+++ Clase 2 - Primera aplicación/Clase 2 - Primera aplicación/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_2___Primera_aplicación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a c-sharp!");
+        }
+    }
+}
---
+++ Clase 3-7 Sintaxis basica/Clase 3-7 Sintaxis basica/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Clase_3_7_Sintaxis_basica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Conversion de texto a numero. Método: parse (CLASE 7)*/
+            Console.WriteLine("Ingrese primer numero");
+            int numOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese segundo numero");
+            int numTwo = int.Parse(Console.ReadLine());
+            int resultado = numOne + numTwo;
+
+            //Console.ReadLine();Espera el ingreso de algun dato y luego el programa seguirá.
+
+            if (numOne >= 15 && numOne <= 20 && numTwo >= 15 && numTwo <= 20)
+            {
+                Console.WriteLine("El numero que ingreso el usuario es: " + numOne);
+                Console.WriteLine("El numero que ingreso el usuario es: " + numTwo);
+                Console.WriteLine("La suma de los dos numeros es:" + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Fuera de rango");
+            }
+
+            
+
+            /*
+             * CLASE 3-6 APROX.
+             * ----------------
+            int edad = 15;
+            Console.WriteLine("La edad de la variable es:" + edad);
+            Console.WriteLine($"La edad de la variable es:{edad}");//Interpolación de STRINGS
+            */
+        }
+    }
+}
