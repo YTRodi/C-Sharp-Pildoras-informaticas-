f26546d2f384663ad14a2d25f44bd8bc9f3c1e25
YTRodi <yagorodi@gmail.com>
2019-12-13 22:53:28 -0300

    subo clase 22 excepciones

---
+++ Clase 22 Excepciones/Clase 22 Excepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Clase_22_Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numeroRandom = new Random();
+            int aleatorio = numeroRandom.Next(0, 100);//Min y Max
+            int miNumero;
+            int intentos = 0;
+
+            Console.WriteLine("Ingresa un n° entre 0 y 100");
+            Console.WriteLine("----------------------------");
+
+            do
+            {
+                intentos++;
+
+                try
+                {
+                    miNumero = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("No ingresaste un valor númerico válido\nSe tomara numero introducido el 0");
+                    miNumero = 0;
+                }
+
+                if (miNumero > aleatorio) Console.WriteLine("El n° es mas bajo");
+                if (miNumero < aleatorio) Console.WriteLine("El n° es mas alto");
+            } while (aleatorio != miNumero);
+
+            Console.WriteLine($"El numero ({miNumero}) es correcto!");
+            Console.WriteLine($"Lo has hecho en {intentos} intentos");
+        }
+    }
+}
---
+++ Clase 9 - 10 metodos/Clase 9 - 10 metodos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Clase_9___10_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            int tam = 0;
+            float acumulador = 0;
+            float contador = 0;
+            float contadorDeCeros = 0;
+
+            Console.WriteLine("Ingrese cuantos numeros quiere ingresar:");
+            tam = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<tam;i++)
+            {
+                Console.WriteLine("Ingrese numero:");
+                numero = float.Parse(Console.ReadLine());
+                contador++;
+                acumulador = acumulador + numero;
+                if(numero==0)
+                { 
+                    contadorDeCeros++;
+                }
+            }
+            
+            Console.WriteLine($"Contador es: {contador}");
+            Console.WriteLine($"Contador de ceros: {contadorDeCeros}");
+            Console.WriteLine($"Acumulador es: {acumulador}");
+        }
+
+        static void mostrarMensaje()
+        {
+            Console.WriteLine("Hola guachin");
+        }
+
+        static double dividirNumeros(double num1, double num2) => num1 / num2;
+        //ESTO TAMBIEN ES VALIDO... CON EL OPERADOR FLECHA. SE DA POR SUPUESTA EL RETURN.
+
+        
+    }
+
+}
